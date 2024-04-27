e19f51c945a215fc351c612d9d4528c1f44f43c9
felipeB21 <bolgarfelipe@gmail.com>
2024-04-27 20:07:53 -0300

    Installed prisma and unistall turso

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ import dotenv from "dotenv";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
 
+import { PrismaClient } from "@prisma/client";
+
 dotenv.config();
+
+const prisma = new PrismaClient();
 
 const app = express();
 const server = createServer(app);
@@ -16,19 +20,44 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("Connected to socket.io");
 
   socket.on("disconnect", () => {
     console.log("Disconnected from socket.io");
   });
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg.message);
+  socket.on("chat message", async (msg) => {
+    try {
+      const message = await prisma.messages.create({
+        data: {
+          content: msg.message,
+        },
+      });
+      io.emit("chat message", message.content);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+    }
   });
+
+  if (!socket.recovered) {
+    try {
+      const results = await prisma.messages.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      results.forEach((result) => {
+        socket.emit("chat message", result.content, result.id);
+      });
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+    }
+  }
 });
 
 app.use(cors());
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
